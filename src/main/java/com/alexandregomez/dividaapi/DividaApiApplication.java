710fbbaa78ea7b7cd255aec35c7a5345de56b050
Alexandre Gomez <asgomez@msn.com>
2026-09-22 13:38:45 -0300

    Initial commit

---
+++ src/main/java/com/alexandregomez/dividaapi/DividaApiApplication.java
@@ -0,0 +1,13 @@
+package com.alexandregomez.dividaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexandregomez/dividaapi/DividaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandregomez.dividaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
